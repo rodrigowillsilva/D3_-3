155069f396ca1e7d731d9e0162a0ab94b5337687
GuguTaets <d2024007029@unifei.edu.br>
2024-10-09 14:06:19 -0300

    Movement Sensivity
    Alterei a sensibilidade da câmera.

--- project-west/Scripts/PlayerMovement.gd
+++ project-west/Scripts/PlayerMovement.gd
@@ -14,8 +14,8 @@ func _unhandled_input(event: InputEvent) -> void:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		if event is InputEventMouseMotion:
-			neck.rotate_y(-event.relative.x * 0.01)
-			camera.rotate_x(-event.relative.y * 0.01)
+			neck.rotate_y(-event.relative.x * 0.001)
+			camera.rotate_x(-event.relative.y * 0.001)
 			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad (-80), deg_to_rad(60))
 
 func _physics_process(delta: float) -> void:
