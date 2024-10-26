aaa9f2b832ca4dafcdd9e8a6be6cc7371448a494
Gabriel Furlan <gfurlan39@gmail.com>
2024-10-26 11:55:37 -0300

    tutorial added

--- project-west/Scripts/main_menu.gd
+++ project-west/Scripts/main_menu.gd
@@ -10,3 +10,7 @@ func _on_play_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_tutorial_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/tutorial.tscn")
