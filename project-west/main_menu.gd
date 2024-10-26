256ae6766c39ee7ceb5c946009bb10f9b3620cef
Gabriel Furlan <gfurlan39@gmail.com>
2024-10-26 10:17:29 -0300

    main menu added

---
+++ project-west/main_menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+func _on_play_pressed() -> void:
+	
+	#get_tree().change_scene_to_file( colocar o level 1 aqui )
+	
+	pass
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
