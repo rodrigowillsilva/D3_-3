b4471aa59c395576a49b3df01fa1d3871814febb
Gabriel Furlan <gfurlan39@gmail.com>
2024-11-02 12:59:56 -0300

    small fixes
    added some functions to the main menu and tried to fix sound ploblems

--- project-west/Scripts/tutorial.gd
+++ project-west/Scripts/tutorial.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_play_2_pressed() -> void:
-	#get_tree().change_scene_to_file( colocar o level 1 aqui )
+	get_tree().change_scene_to_file("res://Scenes/main_map.tscn")
 	pass
 
 
